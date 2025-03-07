89f91bbf468826337d882dee813799798557720d
Loic Baron <loic.baron@gmail.com>
2025-03-07 16:02:05 +0100

    feat: read rasters add threshold

---
+++ yearly.R
@@ -0,0 +1,23 @@
+library("terra")
+
+src_dir <- "data/LANDSAT_NDWI"
+raster_files <- list.files(
+  normalizePath(src_dir),
+  pattern = "\\.(tif|tiff)$",
+  ignore.case = TRUE,
+  full.names = TRUE
+)
+for (f in raster_files) {
+  cat(f)
+}
+
+land_cover <- terra::rast(f)
+# terra::plot(land_cover)
+
+# Get the year based on filename i.e. 1996_LANDSAT_NDWI.tif
+year <- strsplit(basename(f), "_")[[1]][1]
+
+# Define a threshold (e.g., keep values greater than 0)
+threshold_value <- 0
+binary_raster <- land_cover > threshold_value
+# terra::plot(binary_raster)
