7c48b24b4f487a9fef64e79b2382dc1f4724083f
Loic Baron <loic.baron@gmail.com>
2025-03-07 16:02:37 +0100

    refactor: process in a loop

---
+++ helpers.R
@@ -0,0 +1,62 @@
+# Function to extract the area of a raster within each polygon
+raster_area_within_polygons <- function(rst, polygons) {
+  # Ensure the raster has the correct CRS
+  cat("Setting the CRS of the raster...\n")
+  if (is.na(crs(rst)) || crs(rst) == "") {
+    crs(rst) <- "EPSG:4326" # Assign CRS if it's missing
+  }
+  # Ensure the polygons are in sf format
+  cat("Converting polygons to sf format...\n")
+  if (!inherits(polygons, "sf")) {
+    polygons <- sf::st_as_sf(polygons)
+  }
+  # Check for invalid geometries
+  cat("Checking for invalid geometries...\n")
+  invalid_geometries <- !st_is_valid(polygons)
+
+  # If any invalid geometries are found, you can attempt to fix them
+  cat("Fixing invalid geometries...\n")
+  if (any(invalid_geometries)) {
+    polygons <- st_make_valid(polygons)
+  }
+  # cat("Simplifying polygons...\n")
+  # polygons <- st_simplify(polygons, dTolerance = 100)
+  # Ensure the polygons have an area column
+  if (!"area" %in% colnames(polygons)) {
+    cat("Calculating area of polygons...\n")
+    polygons$area <- units::set_units(st_area(polygons), km^2)
+  }
+
+  # Define the global equal-area projection (Mollweide)
+  target_crs <- "ESRI:54009" # Mollweide Projection
+
+  # Project the raster
+  cat("Projecting the raster...\n")
+  r_projected <- project(rst, target_crs)
+
+  # Project the shapefile
+  cat("Projecting the shapefile...\n")
+  polygons_projected <- st_transform(polygons, crs = target_crs)
+
+  # Compute the area of each raster cell in square kilometers
+  cat("Calculating the area of each raster cell in square kilometers...\n")
+  cell_areas <- cellSize(r_projected, unit = "km", mask = TRUE)
+
+  # Calculate the area of the raster within each polygon
+  areas_within_polygons <- exact_extract(
+    cell_areas, polygons_projected, "sum",
+    default_value = 0,
+    progress = TRUE
+  )
+
+  # Add the area results to the polygons data frame
+  polygons_projected$area_raster_km2 <- areas_within_polygons
+
+  # Transform back to EPSG:4326
+  polygons_final <- st_transform(polygons_projected, crs = 4326)
+
+  # Calculate the area ratio
+  polygons_final$val <- polygons_final$area_raster_km2 / polygons_final$area
+
+  return(polygons_final)
+}
--- yearly.R
+++ yearly.R
@@ -8,16 +8,15 @@ raster_files <- list.files(
   full.names = TRUE
 )
 for (f in raster_files) {
-  cat(f)
+  cat(basename(f))
+  land_cover <- terra::rast(f)
+  # terra::plot(land_cover)
+
+  # Get the year based on filename i.e. 1996_LANDSAT_NDWI.tif
+  year <- strsplit(basename(f), "_")[[1]][1]
+
+  # Define a threshold (e.g., keep values greater than 0)
+  threshold_value <- 0
+  binary_raster <- land_cover > threshold_value
+  # terra::plot(binary_raster)
 }
-
-land_cover <- terra::rast(f)
-# terra::plot(land_cover)
-
-# Get the year based on filename i.e. 1996_LANDSAT_NDWI.tif
-year <- strsplit(basename(f), "_")[[1]][1]
-
-# Define a threshold (e.g., keep values greater than 0)
-threshold_value <- 0
-binary_raster <- land_cover > threshold_value
-# terra::plot(binary_raster)
