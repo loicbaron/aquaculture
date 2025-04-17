70a49d93cea6474f0d121bd472578f7bfe46c877
Loic Baron <loic.baron@gmail.com>
2025-04-17 23:50:15 +0200

    feat: update yearly result

--- yearly.R
+++ yearly.R
@@ -1,3 +1,31 @@
+# -------------------------------------------------------
+# FOR MAC OS users
+# -------------------------------------------------------
+# brew reinstall gcc
+# brew install gdal
+# https://github.com/r-lib/usethis/issues/1970#issuecomment-2471529856
+# brew install libgit2
+# brew install udunits
+
+install.packages("sf", configure.args = c(
+    "--with-proj-include=/opt/homebrew/include",
+    "--with-proj-lib=/opt/homebrew/lib",
+    "--with-sqlite3-lib=/opt/homebrew/lib",
+    "--with-sqlite3-include=/opt/homebrew/include"
+))
+install.packages("terra", configure.args = c(
+    "--with-proj-include=/opt/homebrew/include",
+    "--with-proj-lib=/opt/homebrew/lib",
+    "--with-sqlite3-lib=/opt/homebrew/lib",
+    "--with-sqlite3-include=/opt/homebrew/include"
+))
+# -------------------------------------------------------
+
+install.packages(
+  c("dplyr", "exactextractr", "raster", "sf", "terra"),
+  dependencies = TRUE
+)
+install.packages("exactextractr")
 source("helpers.R")
 library(dplyr)
 library(exactextractr)
@@ -5,6 +33,7 @@ library(raster)
 library(sf)
 library(terra)
 
+mkdirs("data/LANDSAT_NDWI_binary")
 src_dir <- "data/LANDSAT_NDWI"
 raster_files <- list.files(
   normalizePath(src_dir),
@@ -12,35 +41,43 @@ raster_files <- list.files(
   ignore.case = TRUE,
   full.names = TRUE
 )
+
 admin_roi <- st_read("data/ADMIN/villages_bgd.gpkg") %>%
   dplyr::filter(adm2_name == "Khulna")
 
-# Initialize an empty list to store results
-results_list <- list()
+keep_cols <- c(
+  'geo_id',
+  "adm1_name",
+  "adm2_name",
+  "adm3_name",
+  "adm4_name",
+  "country_iso3",
+  "area"
+)
+
+results <- admin_roi[, keep_cols]
 
 for (f in raster_files) {
   cat("Processing:", basename(f), "\n")
   land_cover <- terra::rast(f)
   # terra::plot(land_cover)
+  # Define a threshold (e.g., keep values greater than 0)
+  threshold_value <- 0
+  binary_raster <- as.numeric(land_cover > threshold_value)
+  binary_raster[binary_raster == 0] <- NA
+  terra::plot(binary_raster)
 
   # Get the year based on filename i.e. 1996_LANDSAT_NDWI.tif
   year <- as.numeric(strsplit(basename(f), "_")[[1]][1])
 
-  # Define a threshold (e.g., keep values greater than 0)
-  threshold_value <- 0
-  binary_raster <- land_cover > threshold_value
-  # terra::plot(binary_raster)
   result <- raster_area_within_polygons(binary_raster, admin_roi)
   # Optional: Visualize
   terra::plot(result["val"])
 
   # Convert result to data frame and add year column
-  result_df <- as.data.frame(result) %>%
-    dplyr::mutate(year = year)
-
-  # Store in list
-  results_list[[as.character(year)]] <- result_df
+  results[[paste0("aqua_area_", year)]] <- result[["area_raster_km2"]] %>% st_drop_geometry()
+  results[[paste0("aqua_pct_", year)]] <- result[["val"]] %>% st_drop_geometry()
 }
 
-# Combine all results into a single data frame
-final_results <- dplyr::bind_rows(results_list)
+st_write(results, "output/results_khulna.gpkg")
+write.csv(results %>% st_drop_geometry(), "output/my_data.csv")
